af788d2ca3eb0178d0a20cce0cd4eac7fe772e8b
grebenshchikovr <grebenshchikov.r@gmail.com>
2016-10-27 17:31:53 +0300

    add title to Post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161027124115) do
+ActiveRecord::Schema.define(version: 20161027130207) do
 
   create_table "comments", force: true do |t|
     t.text     "content"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20161027124115) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title"
   end
 
 end
